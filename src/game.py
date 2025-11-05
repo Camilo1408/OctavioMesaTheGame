8a5ef2f3f2025e1c8339a42496e0c20077ab09f9
Camilo1408 <121272695+Camilo1408@users.noreply.github.com>
2025-11-05 17:36:51 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -10,7 +10,7 @@ class Game:
         self.screen_width = 800
         self.screen_height = 600
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        pygame.display.set_caption("Roguelike Survival")
+        pygame.display.set_caption("The Epic Feat Of Octavio Mesa")
         self.clock = pygame.time.Clock()
         self.running = True
 
